675ca9d3e875af411848f7de8ee692f497ad1104
linggaadit <48824083+linggaadit@users.noreply.github.com>
2025-09-26 16:55:16 +0700

    Update script.js
    update peta darkmode

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const apiKey = '1a610303c3b5897c08b780b0aeb7e46a';
 const map = L.map('map').setView([-7.3942, 112.7338], 15);
 const markerGroup = L.featureGroup().addTo(map);
 
-// Tambahkan layer peta Stadia Maps Alidade Smooth Dark
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20
 }).addTo(map);
 
 const detailsPanel = document.getElementById('details-content');
@@ -162,4 +162,5 @@ function displayDetails(data, apiData) {
     }
 
 }
+
 
